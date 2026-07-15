0947c030ba6e272bc555fb5944320f52bb2d5be2
riuy0848-del <riuy0848@gmail.com>
2026-07-15 08:05:26 +0000

    Add files via upload

---
+++ firebase-config (1).js
@@ -0,0 +1,12 @@
+/* ===========================================================
+   FIREBASE CONFIG — Gaja College
+   =========================================================== */
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyATSNgBTUww9jj57K31hVb2ioxGTZNB99I",
+  authDomain: "gaja-college.firebaseapp.com",
+  projectId: "gaja-college",
+  storageBucket: "gaja-college.firebasestorage.app",
+  messagingSenderId: "483828479737",
+  appId: "1:483828479737:web:77337f3ab4b78f13f31428"
+};
